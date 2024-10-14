43ec91acb15c86b7a9b2d5cf7ca00479a2a95937
Ludmila F <liuska86@list.ru>
2024-10-14 16:29:29 +0300

    Добавьте файлы проекта.

---
+++ 0502.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include "Table.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	const int ROWS = 5;
+	const int COLS = 5;
+
+	Table<int> table{ ROWS, COLS };
+	table.get_size();
+	try
+	{
+		for (int i = 0; i < ROWS; ++i)
+		{
+			for (int j = 0; j < COLS; ++j)
+			{
+				table[i][j] = (i + 1) * (j + 1);
+				std::cout << table[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+	catch (std::out_of_range& err)
+	{
+		std::cout << "Error! " << err.what() << std::endl;
+	}
+	return 0;
+}
+
+
